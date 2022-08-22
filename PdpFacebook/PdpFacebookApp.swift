35ec9fec8ccfa85ebe187765be75e8190c8e349d
habibi <habibulloherkinov@gmail.com>
2022-08-22 09:13:28 +0500

    Initial Commit

---
+++ PdpFacebook/PdpFacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdpFacebookApp.swift
+//  PdpFacebook
+//
+//  Created by Habibulloh on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PdpFacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
